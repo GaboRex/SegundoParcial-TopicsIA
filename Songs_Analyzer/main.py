3734cb6b7c67edb44b1de2118ec813bcd70d13f0
Gabriel Neme <gaboneme11@gmail.com>
2023-12-04 17:18:39 -0400

    Fix some bugs

--- Songs_Analyzer/main.py
+++ Songs_Analyzer/main.py
@@ -1,29 +1,82 @@
 from fastapi import FastAPI, HTTPException
-from analysis_sentiment_spacy import (
-    SongAnalysis,
-    analyze_sentiment_endpoint,
-    detailed_analysis_endpoint,
-)
-from status import get_status  
+from typing import List
+from pydantic import BaseModel
+from status import get_status
+from sentiment import analyze_sentiment
+from sentiment_analysis import extract_title_and_artist, perform_spacy_analysis
+from newspaper import Article
+
 
 app = FastAPI(title="Song Sentiment Analyzer")
+
+class SongAnalysis(BaseModel):
+    urls: List[str]
+
+predictions = []
 
 
 @app.get("/status")
 def get_app_status():
-    return get_status() 
-
+    return get_status()
 
 @app.post("/Sentiment")
-def analyze_sentiment(song_data: SongAnalysis, language: str = 'es'):
-    return analyze_sentiment_endpoint(song_data, language)
+def analyze_sentiment_endpoint(song_data: SongAnalysis, language: str = 'es'):
+    urls = song_data.urls
 
+    if not urls:
+        raise HTTPException(status_code=400, detail="La lista de URLs no puede estar vacía.")
+
+    results = []
+
+    for url in urls:
+        metadata = extract_title_and_artist(url, language=language)
+
+        if not metadata["info"]:
+            raise HTTPException(status_code=400, detail=f"No se pudo extraer información de la URL: {url}")
+
+        article = Article(url)
+        article.download()
+        article.parse()
+        lyrics = article.text
+
+        if not lyrics.strip():
+            raise HTTPException(status_code=400, detail=f"No se pudo extraer texto de la URL: {url}")
+
+        sentiment_info = analyze_sentiment(lyrics, language=language)
+        results.append({"url": url, "info": metadata["info"], "sentiment": sentiment_info})
+
+    return results
 
 @app.post("/analysis")
-def detailed_analysis(song_data: SongAnalysis, language: str = 'es'):
-    return detailed_analysis_endpoint(song_data, language)
+def detailed_analysis_endpoint(song_data: SongAnalysis, language: str = 'es'):
+    urls = song_data.urls
 
+    if not urls:
+        raise HTTPException(status_code=400, detail="La lista de URLs no puede estar vacía.")
 
+    results = []
+
+    for url in urls:
+        metadata = extract_title_and_artist(url, language=language)
+
+        if not metadata["info"]:
+            raise HTTPException(status_code=400, detail=f"No se pudo extraer información de la URL: {url}")
+
+        article = Article(url)
+        article.download()
+        article.parse()
+        lyrics = article.text
+
+        if not lyrics.strip():
+            raise HTTPException(status_code=400, detail=f"No se pudo extraer texto de la URL: {url}")
+
+        sentiment_info = analyze_sentiment(lyrics, language=language)
+        spacy_analysis = perform_spacy_analysis(lyrics, language=language)
+
+        results.append({"url": url, "info": metadata["info"], "sentiment": sentiment_info, "spacy_analysis": spacy_analysis})
+
+    return results
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True)
---
+++ Songs_Analyzer/sentiment_analysis.py
@@ -0,0 +1,48 @@
+# analysis_helpers.py
+from newspaper import Article
+from bs4 import BeautifulSoup
+import spacy
+import requests
+
+nlp_es = spacy.load("es_core_news_md")
+nlp_en = spacy.load("en_core_web_sm")
+
+def extract_title_and_artist(url, language='es'):
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    text = article.text
+    nlp = nlp_es if language == 'es' else nlp_en
+
+    doc = nlp(text)
+    title_entities = [ent.text for ent in doc.ents if ent.label_ == 'TITLE']
+    title = title_entities[0] if title_entities else None
+
+    if not title:
+        title = extract_title_with_beautifulsoup(url)
+
+    return {"info": title}
+
+def extract_title_with_beautifulsoup(url):
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    title_tag = soup.find('title')
+
+    if title_tag:
+        return title_tag.text
+
+    return None
+
+def perform_spacy_analysis(text, language='es'):
+    nlp = nlp_es if language == 'es' else nlp_en
+    doc = nlp(text)
+
+    pos_tags = [{'text': token.text, 'pos': token.pos_} for token in doc]
+    ner_tags = [{'text': ent.text, 'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_} for ent in doc.ents]
+
+    embedding = doc.vector.tolist()
+
+    return {"pos_tags": pos_tags, "ner_tags": ner_tags, "embedding": embedding}
